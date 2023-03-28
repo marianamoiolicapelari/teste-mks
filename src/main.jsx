8e5cc5b136aada7f24728d8038d0405bca6608da
Mariana M.C <marianamoioli@gmail.com>
2023-03-28 16:37:53 -0300

    Componentizing the routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import GlobalStyled from './styles/globalStyles'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './components/Home'
-import { About } from './components/about'
 import { Menu } from './components/Menu'
+import MyRoutes from './routes'
 import { Footer } from './components/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <BrowserRouter>
+  
       <GlobalStyled />
       <Menu />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
+      <MyRoutes />
       <Footer />
-    </BrowserRouter>
+
   </>,
 )
